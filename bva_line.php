598ea88fbc11b9565806eea964d6bd51189787a1
Eric Betts <bettse@gmail.com>
2010-05-30 17:07:06 -0700

    Adding a balance vs running average as a series of line graphs

---
+++ bva_line.php
@@ -0,0 +1,95 @@
+<?php
+
+include 'include.php';
+
+$budget = ' -w -F "%-A\t%t\n" -M -p "next month" --forecast "d<=[next month]" reg ^exp  -discretionary ';
+
+exec("$ledger $budget", $output);
+
+foreach ($output as $line){
+    //make into key-value pairs
+    $tmp = explode("\t", $line);
+    $categorylist[] = $tmp[0];
+    $budgetlist[$tmp[0]] = $tmp[1] * 1;
+}
+
+$labellist = Array();
+$max = 0;
+$min = 0;
+
+
+foreach($categorylist as $key){
+
+    $tmp = substr(strrchr($key, ":"), 1);
+    $pos = strpos($tmp, " ");
+    if($pos > 0){
+        $parameter= substr($tmp, 0, $pos) . " ";
+    }else{
+        $parameter= $tmp . " ";
+    }
+    
+    $averages = "$ledger -w -F '%D\\t%T\\n' -E -MA -c reg $key | sed -e 's/USD//g'";
+    
+    unset($output);
+    exec("$averages", $output);
+
+    foreach ($output as $line){
+        //make into key-value pairs
+        $tmp = explode("\t", $line);
+        $diff = ($tmp[1]*1) - ($budgetlist[$key]*1); 
+        $datalist[$key][] = $diff;
+        
+        if(!in_array($tmp[0], $labellist)){
+            $labellist[] = $tmp[0];    
+        }
+        if($diff > $max){
+            $max = $diff;
+        }
+        if($diff < $min){
+            $min = $diff;
+        }
+    }
+
+
+}
+
+//print_r($datalist);
+
+
+$title = new title( "Running Average - Budget" );
+
+
+$x_labels = new x_axis_labels();
+$x_labels->rotate(45);
+$x_labels->set_labels( $labellist );
+
+$x = new x_axis();
+$x->set_labels( $x_labels );
+
+$y = new y_axis(); 
+$y->set_range( round($min) * 1.1, round($max) * 1.1);
+$y->set_label_text( "$#val#" );
+
+
+$chart = new open_flash_chart();
+$chart->set_title( $title );
+$chart->set_x_axis( $x );
+$chart->set_y_axis( $y );
+$chart->set_bg_colour( '#FFFFFF' );
+
+
+foreach($categorylist as $category){
+    $l = new line();
+    $l->set_values($datalist[$category]);
+    $l->set_key($category, 12);
+    $l->set_tooltip('<br>$#val#');
+    $color = "#" . substr(md5($category), 0, 6); 
+    $l->set_colour( $color );
+    $chart->add_element($l);
+}
+
+echo $chart->toPrettyString();
+
+
+?>
+
