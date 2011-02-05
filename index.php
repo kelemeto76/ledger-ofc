9b77e87efc3a6cf471129b48eeb03c0bef3f7e24
Eric Betts <bettse@gmail.com>
2011-02-04 23:01:05 -0800

    Adding callback to create/upload images of the graphs

--- index.php
+++ index.php
@@ -2,6 +2,39 @@
 <head>
  
 <script type="text/javascript" src="../js/swfobject.js"></script>
+
+<script type="text/javascript">
+function done(id)
+{
+
+    //alert("Finished upload. Id:"+id);
+}
+
+function post_image(name)
+{
+    debug = false; 
+    url = "http://ataraxia.ericbetts.org/~bettse/ledger-ofc/php-ofc-library/ofc_upload_image.php?name=" + name + ".png";
+    var ofc = findSWF(name);
+    // call our objects image_saved() method when finished
+    x = ofc.post_image(url, 'done', debug );
+}
+ 
+function findSWF(movieName) {
+    if (navigator.appName.indexOf("Microsoft")!= -1) {
+        return window[movieName];
+    } else {
+        return document[movieName];
+    }
+}
+
+function ofc_ready()
+{
+    var t=setTimeout("export_images()", 5000);
+}
+
+</script>
+
+
 <script type="text/javascript">swfobject.embedSWF("open-flash-chart.swf", "month_surplus", "900", "350", "9.0.0", "expressInstall.swf", {"data-file":"month_surplus.php"} );</script>
 <script type="text/javascript">swfobject.embedSWF("open-flash-chart.swf", "year_overview", "900", "350", "9.0.0", "expressInstall.swf", {"data-file":"year_overview.php"} );</script>
 <script type="text/javascript">swfobject.embedSWF("open-flash-chart.swf", "daily_balance", "900", "350", "9.0.0", "expressInstall.swf", {"data-file":"daily_balance.php"} );</script>
@@ -13,21 +46,21 @@ $i = 1;
 if (ini_get('allow_url_fopen') == '1') {
     $script_directory = substr($_SERVER['REQUEST_URI'], 0, strrpos($_SERVER['SCRIPT_FILENAME'], '/'));
     $server = "http://" . $_SERVER['HTTP_HOST'];
-    $acctslist = explode(' ', trim(file_get_contents($server . $script_directory . '/checking_retro.php?listaccts=true')));
+    $acctslist = explode(' ', str_replace(':', '_', trim(file_get_contents($server . $script_directory . '/checking_retro.php?listaccts=true'))));
 } else {
     $acctslist = Array();
 }
 
 if($acctslist == false){ $acctslist = Array();}
-print_r($acctslist);
 
 foreach($acctslist as $acct){
 ?>
 
-<script type="text/javascript">swfobject.embedSWF("open-flash-chart.swf", "<?php echo $acct?>_retrospective", "900", "450", "9.0.0", "expressInstall.swf", {"data-file":"checking_retro.php?acct=<?php echo $i++?>"} );</script>
+<script type="text/javascript">swfobject.embedSWF("open-flash-chart.swf", "<?php echo $acct?>_retrospective", "900", "450", "9.0.0", "expressInstall.swf", {"data-file":"checking_retro.php?acct=<?php echo $i++?>", "id": "<?php echo $acct?>_retrospective"} );</script>
 
 <?php
 }
+
 ?>
 
 </head>
@@ -39,15 +72,25 @@ foreach($acctslist as $acct){
 <div id="daily_balance"></div>
 <div id="year_overview"></div>
 <div id="bva_line"></div>
-<?php
-foreach($acctslist as $acct){
-?>
+<?php foreach($acctslist as $acct){ ?>
 <div id="<?php echo $acct?>_retrospective"></div>
+<?php } ?>
 
-<?php
+<script type="text/javascript">
+function export_images()
+{
+<?php foreach($acctslist as $acct){ ?>
+    post_image("<?php echo $acct?>_retrospective");
+<?php } ?>
+    post_image("month_surplus");
+    post_image("month_breakdown");
+    post_image("daily_balance");
+    post_image("year_overview");
+    post_image("bva_line");
 }
-?>
+</script>
 
  
 </body>
 </html>
+
