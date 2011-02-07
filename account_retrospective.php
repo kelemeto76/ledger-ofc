3ca16c0eac04d6627dc0d3e83c9edcd68d4af1a3
Eric Betts <bettse@gmail.com>
2011-02-06 20:29:17 -0800

    Changing from checking_retro to account_retrospective

---
+++ account_retrospective.php
@@ -0,0 +1,53 @@
+<?php
+
+include 'include.php';
+
+if (isset($_REQUEST['acct'])) {
+    $acct = filter_var($_REQUEST['acct'], FILTER_SANITIZE_MAGIC_QUOTES);
+    $acct = str_replace('_', ':', $acct); 
+
+    $bal_cmd = '-w -J -d "d<=[today] & d>=[today]-365" --weekly --sort d reg "' . $acct . '"'; 
+    exec("$ledger $bal_cmd", $output);
+
+    foreach ($output as $line){
+        $tmp = explode(" ", $line);
+        $datalist[] = 1*$tmp[1];
+        $labellist[] = $tmp[0];
+    }
+
+    $title = new title( $acct . " Retrospective" );
+
+    $default_dot = new dot();
+    $default_dot->tooltip( '$#val#' );
+
+    $line = new line();
+    $line->set_values( $datalist );
+    $line->set_default_dot_style($default_dot);
+
+    $x_labels = new x_axis_labels();
+    $x_labels->rotate(45);
+    $x_labels->set_labels( $labellist );
+    $x_labels->set_steps(3);
+
+    $x = new x_axis();
+    $x->set_labels( $x_labels );
+
+    $y = new y_axis(); 
+    $y->set_range( min(min($datalist)-10, 0) , round(max($datalist) * 1.1));
+    $y->set_label_text( "$#val#" );
+
+
+    $chart = new open_flash_chart();
+    $chart->set_title( $title );
+    $chart->add_element( $line );
+    $chart->set_x_axis( $x );
+    $chart->set_y_axis( $y );
+    $chart->set_bg_colour( '#FFFFFF' );
+
+    echo $chart->toPrettyString();
+
+}
+
+
+?>
+
--- index.php
+++ index.php
@@ -3,66 +3,24 @@
  
 <script type="text/javascript" src="../js/swfobject.js"></script>
 
-<script type="text/javascript">
-function done(id)
-{
-
-    //alert("Finished upload. Id:"+id);
-}
-
-function post_image(name)
-{
-    debug = false; 
-    url = "http://localhost/~bettse/ledger-ofc/php-ofc-library/ofc_upload_image.php?name=" + name + ".png";
-    var ofc = findSWF(name);
-    // call our objects image_saved() method when finished
-    x = ofc.post_image(url, 'done', debug );
-}
- 
-function findSWF(movieName) {
-    if (navigator.appName.indexOf("Microsoft")!= -1) {
-        return window[movieName];
-    } else {
-        return document[movieName];
-    }
-}
-
-function ofc_ready()
-{
-    var t=setTimeout("export_images()", 5000);
-}
-
-</script>
-
-
 <script type="text/javascript">swfobject.embedSWF("open-flash-chart.swf", "month_surplus", "900", "350", "9.0.0", "expressInstall.swf", {"data-file":"month_surplus.php"} );</script>
 <script type="text/javascript">swfobject.embedSWF("open-flash-chart.swf", "year_overview", "900", "350", "9.0.0", "expressInstall.swf", {"data-file":"year_overview.php"} );</script>
 <script type="text/javascript">swfobject.embedSWF("open-flash-chart.swf", "daily_balance", "900", "350", "9.0.0", "expressInstall.swf", {"data-file":"daily_balance.php"} );</script>
 <script type="text/javascript">swfobject.embedSWF("open-flash-chart.swf", "month_breakdown", "600", "600", "9.0.0", "expressInstall.swf", {"data-file":"month_breakdown.php"} );</script>
 <script type="text/javascript">swfobject.embedSWF("open-flash-chart.swf", "bva_line", "900", "450", "9.0.0", "expressInstall.swf", {"data-file":"bva_line.php"} );</script>
 <?php
-$i = 1;
-
-if (ini_get('allow_url_fopen') == '1') {
-    $script_directory = substr($_SERVER['REQUEST_URI'], 0, strrpos($_SERVER['SCRIPT_FILENAME'], '/'));
-    $server = "http://" . $_SERVER['HTTP_HOST'];
-    $acctslist = explode(' ', str_replace(':', '_', trim(file_get_contents($server . $script_directory . '/checking_retro.php?listaccts=true'))));
-} else {
-    $acctslist = Array();
-}
-
-if($acctslist == false){ $acctslist = Array();}
-
+$acctslist = Array();
+$acctslist[] = "OSUFed:Checking";
+$acctslist[] = "OSUFed:Savings";
+$acctslist[] = "FirstTech:Checking";
+$acctslist[] = "FirstTech:Savings";
 foreach($acctslist as $acct){
+$acct = str_replace(':', '_', $acct); 
 ?>
 
-<script type="text/javascript">swfobject.embedSWF("open-flash-chart.swf", "<?php echo $acct?>_retrospective", "900", "450", "9.0.0", "expressInstall.swf", {"data-file":"checking_retro.php?acct=<?php echo $i++?>", "id": "<?php echo $acct?>_retrospective"} );</script>
+<script type="text/javascript">swfobject.embedSWF("open-flash-chart.swf", "<?php echo $acct ?>_retrospective", "900", "450", "9.0.0", "expressInstall.swf", {"data-file":"account_retrospective.php?acct=<?php echo $acct ?>"} );</script>
 
-<?php
-}
-
-?>
-
+<?php } ?>
 </head>
 <body>
  
@@ -72,24 +30,11 @@ foreach($acctslist as $acct){
 <div id="daily_balance"></div>
 <div id="year_overview"></div>
 <div id="bva_line"></div>
-<?php foreach($acctslist as $acct){ ?>
-<div id="<?php echo $acct?>_retrospective"></div>
+<?php foreach($acctslist as $acct){ 
+$acct = str_replace(':', '_', $acct); 
+?>
+<div id="<?php echo $acct ?>_retrospective"></div>
 <?php } ?>
-
-<script type="text/javascript">
-function export_images()
-{
-<?php foreach($acctslist as $acct){ ?>
-    post_image("<?php echo $acct?>_retrospective");
-<?php } ?>
-    post_image("month_surplus");
-    post_image("month_breakdown");
-    post_image("daily_balance");
-    post_image("year_overview");
-    post_image("bva_line");
-}
-</script>
-
  
 </body>
 </html>
